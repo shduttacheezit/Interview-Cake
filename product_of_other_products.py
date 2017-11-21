e66b4e1af1a8deb8865cde1de781f78a2e5c953a
Shilpa Dutta <dutta.shilpa6@gmail.com>
2017-11-20 21:54:31 -0800

    interview cake 2

---
+++ product_of_other_products.py
@@ -0,0 +1,43 @@
+def prod_of_others(nums):
+    """
+    You have a list of integers, and for each index you want to 
+    find the product of every integer except the integer at that index.
+
+    >>>prod_of_others([4])
+    IndexError: array should be at least 2 numbers
+
+    >>>prod_of_others([3, 4, 9, 2])
+    [72, 54, 24, 108]
+
+    """
+    
+
+    if len(nums) < 2:
+        raise IndexError('array should be at least 2 numbers')
+    
+    new_arr = [None] * len(nums)
+    
+    prod_at_index = 1
+    i = 0
+    while i < len(nums):
+        new_arr[i] = prod_at_index
+        prod_at_index *= nums[i]
+        i += 1
+    
+    prod_at_index = 1
+    i = len(nums) - 1
+    while i >= 0:
+        new_arr[i] *= prod_at_index
+        prod_at_index *= nums[i]
+        i -= 1
+    
+    return new_arr
+    
+    # return 'running with %s' % arg
+
+# run your function through some test cases here
+# remember: debugging is half the battle!
+print prod_of_others([8, 10, 2])
+print prod_of_others([2, 3, 4, 7])
+print prod_of_others([3])
+print prod_of_others([3, 4, 9, 2])
